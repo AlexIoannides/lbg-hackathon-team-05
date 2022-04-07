0013570b4ecc0d7ed2c386351b40d3b91722ca45
alexioannides <alex.ioanndies@yahoo.co.uk>
2022-04-08 00:28:32 +0100

    Add test endpoint script

---
+++ example_pipeline/test_endpoint.py
@@ -0,0 +1,56 @@
+from typing import Dict, List, Union
+
+from dotenv import load_dotenv
+from google.cloud import aiplatform
+from google.protobuf import json_format
+from google.protobuf.struct_pb2 import Value
+
+from train_and_deploy import PROJECT_ID
+
+
+def predict_custom_trained_model_sample(
+    project: str,
+    endpoint_id: str,
+    instances: Union[Dict, List[Dict]],
+    location: str = "us-central1",
+    api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+):
+    """
+    `instances` can be either single instance of type dict or a list
+    of instances.
+    """
+    # The AI Platform services require regional API endpoints.
+    client_options = {"api_endpoint": api_endpoint}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+    # The format of each instance should conform to the deployed model's prediction input schema.
+    instances = instances if type(instances) == list else [instances]
+    instances = [
+        json_format.ParseDict(instance_dict, Value()) for instance_dict in instances
+    ]
+    parameters_dict = {}
+    parameters = json_format.ParseDict(parameters_dict, Value())
+    endpoint = client.endpoint_path(
+        project=project, location=location, endpoint=endpoint_id
+    )
+    response = client.predict(
+        endpoint=endpoint, instances=instances, parameters=parameters
+    )
+    print("response")
+    print(" deployed_model_id:", response.deployed_model_id)
+    # The predictions are a google.protobuf.Value representation of the model's predictions.
+    predictions = response.predictions
+    for prediction in predictions:
+        print(" prediction:", dict(prediction))
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    predict_custom_trained_model_sample(
+        project=PROJECT_ID,
+        endpoint_id="7451882882647195648",
+        location="us-central1",
+        instances={"instance_key_1": 0.5}
+    )
